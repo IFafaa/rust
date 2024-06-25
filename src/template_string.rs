cf3bfbeff79b41743ee40125f8eb9c8ac2561afb
Fabricio Lima <fabriciotrab2005@gmail.com>
2024-06-25 00:42:07 -0300

    doc: template strings

---
+++ src/template_string.rs
@@ -0,0 +1,7 @@
+fn generic_function_template_string(){
+    let hours = 160;
+    println!("I work {} hours", hours); // to use template string!
+
+    let sprint_time_in_days = 14;
+    println!("I work {} hours, and my sprint lifetime it's {} days", hours, sprint_time_in_days)
+}
